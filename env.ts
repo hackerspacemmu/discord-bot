a57da79a821b167e3dab48d58aeccd9d7d4b0028
Tharshen2124 <tharshensurianbalan@gmail.com>
2025-12-18 02:23:48 +0800

    feat: setup eslint, discord client and env variables

---
+++ env.ts
@@ -0,0 +1,4 @@
+export const discordBotApplicationId = process.env.DISCORD_BOT_APPLICATION_ID as string
+export const discordBotPublicKey = process.env.DISCORD_BOT_PUBLIC_KEY as string
+export const discordBotToken = process.env.DISCORD_BOT_TOKEN as string
+export const discordBotOauth2GeneratedUrl = process.env.DISCORD_BOT_OAUTH2_GENERATED_URL as string
