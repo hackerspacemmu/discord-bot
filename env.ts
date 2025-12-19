75a21ff621c94e0310fb3d275a200bc57d9e2567
Tharshen2124 <tharshensurianbalan@gmail.com>
2025-12-19 23:01:37 +0800

    feat: add discord client

--- env.ts
+++ env.ts
@@ -1,3 +1,7 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 export const discordBotApplicationId = process.env.DISCORD_BOT_APPLICATION_ID as string
 export const discordBotPublicKey = process.env.DISCORD_BOT_PUBLIC_KEY as string
 export const discordBotToken = process.env.DISCORD_BOT_TOKEN as string
