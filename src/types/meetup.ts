9d96164792ae582b508039a67a71306e6d76e6d4
Tharshen2124 <tharshensurianbalan@gmail.com>
2026-02-13 03:42:56 +0800

    feat: add search meetup by date command

--- src/types/meetup.ts
+++ src/types/meetup.ts
@@ -1,6 +1,8 @@
 export interface Meetup {
     "id": number,
     "date": string,
-    "number": number,
+    "number": number | null,
+    "hackathon_number": number | null,
+    "host_id": number,
     "category": string  
 }
